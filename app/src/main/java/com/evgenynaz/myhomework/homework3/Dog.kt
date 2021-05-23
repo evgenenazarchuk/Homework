9d55c9517c954484a5c66bfa4df3d0d6a3d9221c
e.nazarchuk <e.nazarchuk@gmail.com>
2021-05-23 15:17:51 +0300

    homework3

---
+++ app/src/main/java/com/evgenynaz/myhomework/homework3/Dog.kt
@@ -0,0 +1,5 @@
+package com.evgenynaz.myhomework.homework3
+
+class Dog(val name: String,speed: Double) : Animal(speed) {
+
+}
---
+++ app/src/main/java/com/evgenynaz/myhomework/homework3/Elephant.kt
@@ -0,0 +1,4 @@
+package com.evgenynaz.myhomework.homework3
+
+class Elephant(val name: String,speed: Double) : Animal(speed) {
+}
