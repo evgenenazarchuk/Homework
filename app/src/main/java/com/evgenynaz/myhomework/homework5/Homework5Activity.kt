c03b4b97dff0cd29e04348affd059ed6b0554acd
e.nazarchuk <e.nazarchuk@gmail.com>
2021-06-02 20:55:08 +0300

    homework5

--- app/src/main/java/com/evgenynaz/myhomework/homework5/Homework5Activity.kt
+++ app/src/main/java/com/evgenynaz/myhomework/homework5/Homework5Activity.kt
@@ -1,10 +1,11 @@
-package com.evgenynaz.myhomework
+package com.evgenynaz.myhomework.homework5
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.evgenynaz.myhomework.R
 
-class Homework6Activity: AppCompatActivity() {
+class Homework5Activity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_homework6)
+        setContentView(R.layout.activity_homework5)
     }
 }
